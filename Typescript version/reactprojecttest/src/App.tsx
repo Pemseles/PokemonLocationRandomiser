65d31fa664bbcca15ddd4bc3adfcda55771aa4a8
Pemseles <TJBouwsema@gmail.com>
2022-01-11 16:55:11 +0100

    making progress w settings
    aaaa

--- Typescript version/reactprojecttest/src/App.tsx
+++ Typescript version/reactprojecttest/src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css';
 import * as React from 'react';
+
+import * as canonLocations from './pokemondata/wildlocations/canonlocations.json';
+
 import {
     CssBaseline,
     Box,
@@ -14,8 +17,15 @@ import {
     Button,
     List,
     ListItem,
+    FormGroup,
+    FormLabel,
+    FormControlLabel,
 
 } from '@material-ui/core';
+import {
+    Autocomplete
+} from '@material-ui/lab';
+import { json } from 'stream/consumers';
 
 function App() {
 
@@ -23,23 +33,26 @@ function App() {
     const [genSetting, setGenSetting] = React.useState("");
     const [monCount, setMonCount] = React.useState("");
     const [regionPref, setRegionPref] = React.useState("");
-    const [specificRegion, setSpecificRegion] = React.useState("");
     const [locationPref, setLocationPref] = React.useState("");
-    const [regionList, setRegionList] = React.useState(Array<string>());
+    const [regionBoolList, setRegionBoolList] = React.useState(Array<[string, boolean]>());
     const [regionSelectBool, setRegionSelectBool] = React.useState(false);
-    const [locationList, setLocationList] = React.useState(Array<string>());
+    const [locationBoolList, setLocationBoolList] = React.useState(Array<Array<[string, boolean]>>());
     const [locationSelectBool, setLocationSelectBool] = React.useState(false);
+    const [selectedRegions, setSelectedRegions] = React.useState(Array<string>());
+    const [selectedLocations, setSelectedLocations] = React.useState(Array<Array<string>>());
 
     const genButtonBoolRef = React.useRef(null);
     const genSettingRef = React.useRef(null);
     const monCountRef = React.useRef(null);
     const regionPrefRef = React.useRef(null);
-    const specificRegionRef = React.useRef(null);
     const locationPrefRef = React.useRef(null);
     const regionListRef = React.useRef(null);
     const regionSelectBoolRef = React.useRef(null);
     const locationListRef = React.useRef(null);
     const locationSelectBoolRef = React.useRef(null);
+
+    //const multiRegionCondition = chosenRegions.filter((v) => v).length < 1;
+    //const multiLocationCondition = chosenLocations.filter((v) => v).length < 1;
 
     const handleGenButtonBool = (event : any) => {
         console.log(event.target.value);
@@ -57,15 +70,20 @@ function App() {
         console.log(event.target.value);
         setRegionPref(event.target.value);
     }
-    const handleSpecificRegion = (event: any) => {
-        console.log(event.target.value);
-        setSpecificRegion(event.target.value);
-    }
     const handleLocationPref = (event: any) => {
         console.log(event.target.value);
         setLocationPref(event.target.value);
     }
 
+    console.log(genButtonBool);
+    /*
+    React.useEffect(() => {
+        (async () => {
+            setRegionBoolList(await getRegionBools());
+            setLocationBoolList(await getLocationBools());
+        })();
+    }, []);
+*/
     return (
         <React.Fragment>
             <CssBaseline />
@@ -126,10 +144,8 @@ function App() {
                                     onChange={handleRegionPref}
                                     ref={regionPrefRef}
                                 >
-                                    <MenuItem value={"OneRandom"}>One Random</MenuItem>
-                                    <MenuItem value={"ManyRandom"}>Many Random</MenuItem>
-                                    <MenuItem value={"OneSpecific"}>One Specific</MenuItem>
-                                    <MenuItem value={"ManySpecific"}>Many Specific</MenuItem>
+                                    <MenuItem value={"Random"}>Random</MenuItem>
+                                    <MenuItem value={"Specific"}>Specific</MenuItem>
                                 </Select>
                             </FormControl>
                         : ''}
@@ -144,60 +160,95 @@ function App() {
                                     onChange={handleLocationPref}
                                     ref={locationPrefRef}
                                 >
-                                    <MenuItem value={"OneRandom"}>One Random</MenuItem>
-                                    <MenuItem value={"ManyRandom"}>Many Random</MenuItem>
-                                    <MenuItem value={"OneSpecific"}>One Specific</MenuItem>
-                                    <MenuItem value={"ManySpecific"}>Many Specific</MenuItem>
+                                    <MenuItem value={"Random"}>Random</MenuItem>
+                                    <MenuItem value={"Specific"}>Specific</MenuItem>
                                 </Select>
                             </FormControl>
                         : ''}
-                        {genSetting === "Location" && regionPref === "OneSpecific" && locationPref !== "" ?
-                            <FormControl style={{ width: '7%', marginLeft: '50px' }}>
-                                <InputLabel>Specific Region</InputLabel>
-                                <Select
-                                    id="specificRegion"
-                                    labelId='specificRegion'
-                                    label="Specific Region"
-                                    value={specificRegion}
-                                    onChange={handleSpecificRegion}
-                                    ref={specificRegionRef}
-                                >
-                                    <MenuItem value={"Kanto"}>Kanto</MenuItem>
-                                    <MenuItem value={"Johto"}>Johto</MenuItem>
-                                    <MenuItem value={"Hoenn"}>Hoenn</MenuItem>
-                                    <MenuItem value={"Sinnoh"}>Sinnoh</MenuItem>
-                                    <MenuItem value={"Unova"}>Unova</MenuItem>
-                                    <MenuItem value={"Kalos"}>Kalos</MenuItem>
-                                    <MenuItem value={"Alola"}>Alola</MenuItem>
-                                    <MenuItem value={"Galar"}>Galar</MenuItem>
-                                </Select>
-                            </FormControl>
+                        {genSetting === "Location" ?
+                            /*<Autocomplete
+
+                            />*/
+                            true
                         : ''}
                     </Toolbar>
                 </AppBar>
             </Box>
-            {genButtonBool === false ?
-                <div style={{ marginTop: '70px', marginBottom: '70px' }} id="specificsGrid">
-                    <List>
-                        {regionList.map((region, index) => {
-                            if (index % 6 === 0) {
-                                return (
-                                    <><ListItem style={{ alignItems: 'center', justifyContent: 'center', marginRight: '50px' }}>
-                                        
-                                    </ListItem></>
-                                )
-                            }
-                        })}
-                    </List>
-                </div> 
-            : ''}
-            {genButtonBool === true ?
-                <Button variant="contained" color="primary" onClick={() => (console.log("function for generating mons here"))} />
-            : ''}
-            
-            <Divider />
+
+            <AppBar position="fixed" style={{ background: 'white', top: 'auto', bottom: 0, alignItems: 'center' }}>
+                <Toolbar>
+                    {genButtonBool === true ? 
+                        <Button variant="contained" color="primary" onClick={() => (/*Generator(regionPref, regionBoolList, locationPref, locationBoolList)*/ console.log("hi"))}>
+                            Generate Team
+                        </Button>
+                    : 
+                        <Typography variant="h5" style={{ color: 'black', marginLeft: '6px' }}>
+                            Settings incomplete
+                        </Typography>
+                    }
+                    
+                </Toolbar>
+            </AppBar>
         </React.Fragment>
     );
+}
+
+async function getRegions() {
+    const arr = Object.keys(canonLocations);
+    arr.pop()
+    console.log(arr);
+    return arr;
+}
+
+async function getLocations() {
+    const locationArr = Object.values(canonLocations);
+    const regionArr = await getRegions();
+    locationArr.pop();
+
+    let locationArrNew = new Array<Array<string>>();
+    for (let i = 0; i < regionArr.length; i++) {
+        locationArrNew.push(Object.keys(locationArr[i]));
+    }
+
+    console.log(locationArrNew);
+    return locationArrNew;
+}
+/*
+async function getRegionBools() {
+    const regionArr = await getRegions();
+    
+    // get bools for regionList
+    let returnArr = new Array<[string, boolean]>();
+    
+    for (let i = 0; i < regionArr.length; i++) {
+        returnArr.push([regionArr[i], false]);
+    }
+    console.log(returnArr);
+    return returnArr;
+}
+
+async function getLocationBools() {
+    // get bools for locationList
+    let returnArr = new Array<Array<[string, boolean]>>();
+    let pushMe = new Array();
+
+    const regionArr = await getRegions();
+    const locationArr = await getLocations();
+
+    for (let i = 0; i < regionArr.length; i++) {
+        for (let j = 0; j < locationArr[i].length; j++) {
+            pushMe[j] = [locationArr[i][j], false];
+        }
+        returnArr.push(pushMe);
+        pushMe = new Array();
+    }
+
+    console.log(returnArr);
+    return returnArr;
+}
+*/
+function Generator(regionPref : string, regions : Array<[string, boolean]>, locationPref : string, locations : Array<Array<[string, boolean]>>) {
+
 }
 
 export default App;
